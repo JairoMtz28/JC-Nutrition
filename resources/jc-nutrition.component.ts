4510b0361ba55a1add919bfa15832029ac59bc73
JairoMtz28 <119071313+JairoMtz28@users.noreply.github.com>
2025-10-29 11:17:21 -0600

    Add files via upload

---
+++ resources/jc-nutrition.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jc-nutrition',
+  templateUrl: './jc-nutrition.component.html',
+  styleUrls: ['./jc-nutrition.component.css']
+})
+export class JcNutritionComponent {}
